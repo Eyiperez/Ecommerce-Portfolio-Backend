a8bb61426fdbab89a6e4aae974a80775d6e8f1be
Erika <erika@Elizabeths-Air.fios-router.home>
2019-03-17 22:44:03 -0400

    installed coverall

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors');
 
 const { isRequiredsNeeded, } = require('./services/utils')
 
 const app = express();
 
-app.use(bodyParser.json()) 
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false}))
+
+app.use(bodyParser.json())
 
 app.post('/buyer', (req, res) => {
     if (isRequiredsNeeded(req.body)) {
